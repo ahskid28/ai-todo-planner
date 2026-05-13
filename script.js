947f4335b6bb40fd8beb2937d4df44dc7af5adee
ahskid28 <diksha28122020@gmail.com>
2026-05-13 21:54:12 +0530

    Updated AI planner features

--- script.js
+++ script.js
@@ -16,18 +16,22 @@ function addTask() {
 
   let priority = "Low";
 
-const text = (title + " " + desc).toLowerCase();
+  const text = (title + " " + desc).toLowerCase();
 
-if(text.includes("exam") || text.includes("urgent")) {
-  priority = "High";
-}
+  if(text.includes("exam") || text.includes("urgent")) {
+    priority = "High";
+  }
 
-const task = {
-  title,
-  desc,
-  date,
-  priority
-};
+  const category = detectCategory(text);
+
+  const task = {
+    title,
+    desc,
+    date,
+    category,
+    priority,
+    completed: false
+  };
 
   tasks.push(task);
 
@@ -42,17 +46,64 @@ function displayTasks() {
 
   taskList.innerHTML = "";
 
-  tasks.forEach(task => {
+  tasks.forEach((task, index) => {
 
     taskList.innerHTML += `
-      <div class="task">
+     <div class="task ${task.priority.toLowerCase()} ${task.completed ? 'done' : ''}"> 
         <h3>${task.title}</h3>
         <p>${task.desc}</p>
         <p>${task.date}</p>
 <p>Priority: ${task.priority}</p>
+
+<p>Category: ${task.category}</p>
+
+<button onclick="completeTask(${index})">
+  Complete
+</button>
+
+<button onclick="deleteTask(${index})">
+  Delete
+</button>
       </div>
     `;
   });
 }
 
 displayTasks();
+
+function completeTask(index) {
+
+  tasks[index].completed = !tasks[index].completed;
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  displayTasks();
+}
+
+function deleteTask(index) {
+
+  tasks.splice(index, 1);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  displayTasks();
+}
+
+function detectCategory(text) {
+
+  text = text.toLowerCase();
+
+  if(text.includes("exam") || text.includes("study")) {
+    return "Study";
+  }
+
+  if(text.includes("gym")) {
+    return "Health";
+  }
+
+  if(text.includes("project")) {
+    return "Work";
+  }
+
+  return "Personal";
+}
